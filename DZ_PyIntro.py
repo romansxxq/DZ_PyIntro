379a7dd84d1d4afb4240ad4001b266c484798e63
Roman <asssfsa010@gmail.com>
2024-11-16 21:33:30 +0200

    Initial commit

---
+++ DZ_PyIntro.py
@@ -0,0 +1,36 @@
+#Ex.1
+# Enter = int(input("Enter number: "))
+# if (Enter % 2) == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#Ex.2
+# Enter = int(input("Enter number: "))
+# if (Enter % 7) == 0:
+#     print("Number is a multiple of 7")
+# else:
+#     print("Number is not a multiple of 7")
+
+#Ex.3
+# def ManyOrLess():
+#     n1 = int(input("Enter number: "))
+#     n2 = int(input("Enter number: "))
+#     if n1 > n2:
+#         return n1
+#     else:
+#         return n2
+#
+# res = ManyOrLess()
+# print(f'{res} is more')
+
+#Ex.4
+
+n1 = int(input("Enter number: "))
+n2 = int(input("Enter number: "))
+Add= n1 + n2
+print(f'{n1} + {n2} = {Add}')
+print(f'{n1} - {n2} = {n1 - n2}')
+print(f'{n1} * {n2} = {n1 * n2}')
+print(f'Average: {(n1+n2)/2}')
+
